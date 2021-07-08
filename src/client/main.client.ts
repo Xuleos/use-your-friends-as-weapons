9d5ee75e7cf7c0f7c12170ac155c2e22460784d0
Evan <Evancat77@gmail.com>
2021-07-08 17:31:23 -0400

    logger

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,4 +1,21 @@
 import { Flamework } from "@rbxts/flamework";
+import Log, { Logger } from "@rbxts/log";
+import { $git } from "rbxts-transform-debug";
+import Zircon, { ZirconClient } from "@rbxts/zircon";
+
+// eslint-disable-next-line prettier/prettier
+Log.SetLogger(
+	Logger.configure()
+		.WriteTo(Zircon.Log.Console())
+		.WriteTo(Log.RobloxOutput())
+		.EnrichWithProperty("CommitId", $git().Commit)
+		.Create(),
+);
+
+ZirconClient.BindConsole({
+	EnableTags: true,
+	Keys: [Enum.KeyCode.Backquote],
+});
 
 Flamework.addPaths("src/client/controllers", "src/client/components");
 Flamework.ignite();
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,4 +1,16 @@
 import { Flamework } from "@rbxts/flamework";
+import Log, { Logger } from "@rbxts/log";
+import { $git } from "rbxts-transform-debug";
+import Zircon from "@rbxts/zircon";
+
+// eslint-disable-next-line prettier/prettier
+Log.SetLogger(
+	Logger.configure()
+		.WriteTo(Zircon.Log.Console())
+		.WriteTo(Log.RobloxOutput())
+		.EnrichWithProperty("CommitId", $git().Commit)
+		.Create(),
+);
 
 Flamework.addPaths("src/server/services", "src/server/components");
 Flamework.ignite();
